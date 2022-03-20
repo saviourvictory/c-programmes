7c50df22f2809880bc70387261aa5f0e27e7a993
saviourvictory <101803568+saviourvictory@users.noreply.github.com>
2022-03-21 01:04:52 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("SAVIOUR VICTORY\n");
+    printf("HELLO");
+    return 0;
+    
+}
---
+++ sumofnumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+
+printf("NAME= SAVIOUR VICTORY\n");
+printf("enter the value ofa\n");
+scanf("%d",&a);
+printf("enter the value ofb\n");
+scanf("%d",&b);
+c=a+b;
+printf("sum%d\n",c);
+return 0;
+}
